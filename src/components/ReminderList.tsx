0377737e412a6bd5755c0b249becac612339d6c2
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-12 22:49:19 +0400

    Creating a Component Title.

---
+++ src/components/ReminderList.tsx
@@ -0,0 +1,18 @@
+import Reminder from "../models/reminder";
+
+interface ReminderListProps {
+  items: Reminder[];
+}
+
+//Items destructuring
+function ReminderList({ items }: ReminderListProps) {
+  return (
+    <ul>
+      {items.map((item) => (
+        <li key={item.id}>{item.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default ReminderList;
